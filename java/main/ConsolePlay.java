e13ab859a3a94be8f84c2a9438c6864347e24096
zaboople <troymotte@yahoo.com>
2017-02-19 20:17:40 -0600

    Minor

--- java/main/ConsolePlay.java
+++ java/main/ConsolePlay.java
@@ -1,6 +1,38 @@
 package main;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class ConsolePlay {
   public static void main(String[] args) throws Exception {
-    System.out.print(3 % 8);
+    new ConsolePlay().play();
+  }
+
+  private BufferedReader reader;
+  private ConsolePlay() throws Exception {
+    reader=new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  private void play() throws Exception {
+    Game game=new Game(0);
+    while (!game.isOver()){
+      if (game.isWaiting()){
+        game.nextCard();
+        draw(game);
+      }
+      else if (game.isWaitingStriked()) {
+        readLine();
+      }
+      else if (game.isCardUp()) {
+        String place=readLine();
+      }
+    }
+  }
+  private void draw(Game game) throws Exception {
+    AsciiBoard.draw(game.getBoard(), System.out);
+    System.out.println("STRIKES: "+game.getStrikes());
+    System.out.flush();
+  }
+  private String readLine() throws Exception {
+    return reader.readLine();
   }
 }
--- java/main/Game.java
+++ java/main/Game.java
@@ -4,10 +4,17 @@ import java.util.function.Function;
 public class Game {
   private static final int STRIKE_LIMIT=3;
   private static final int KEY_COUNT=2;
-  private static final int WAITING=0, CARD_UP=1, CARD_PLACED=2, LOST=3, WON=4, GIVE_UP=5;
+  private static final int
+    WAITING=0,
+    WAITING_STRIKED=1,
+    CARD_UP=2,
+    CARD_PLACED=3,
+    LOST=4,
+    WON=5,
+    GIVE_UP=6;
 
   private final Board board=new Board();
-  private final Deck deck=new Deck(3, 24, 24, 24, 24);
+  private final Deck deck=new Deck(3, 21, 21, 21, 21);
   private final int bet;
   private Card upCard=null;
   private int state=WAITING;
@@ -21,11 +28,18 @@ public class Game {
     upCard=deck.next();
     if (upCard.isStrike()) {
       strikes++;
-      state=strikes==STRIKE_LIMIT ?LOST :WAITING;
+      state=strikes==STRIKE_LIMIT ?LOST :WAITING_STRIKED;
     } else {
       state=CARD_UP;
     }
     return this;
+  }
+
+  public Board getBoard() {
+    return board;
+  }
+  public Card getUpCard() {
+    return upCard;
   }
 
   public boolean canPutUp() {return board.canPutUp();}
@@ -38,7 +52,13 @@ public class Game {
   public boolean putLeft() {return put(board::putLeft);}
   public boolean putRight() {return put(board::putRight);}
 
+  public void rotateCard() {
+    requireState(CARD_PLACED);
+    board.rotateCard();
+  }
   public void finishPut() {
+    requireState(CARD_PLACED);
+    upCard=null;
     if (board.onKey())
       keys++;
     else if (board.onBonus())
@@ -52,24 +72,38 @@ public class Game {
     else
       state=WAITING;
   }
+  public Game ackStrike() {
+    requireState(WAITING_STRIKED);
+    state=WAITING;
+    return this;
+  }
   public Game giveUp() {
     state=GIVE_UP;
     return this;
   }
-
   private boolean put(Function<Card, Boolean> f) {
-    if (state!=CARD_UP) throw new IllegalStateException("State should be CARD_UP, was "+state);
+    requireState(CARD_UP);
     boolean res=f.apply(upCard);
     if (res) state=CARD_PLACED;
     return res;
   }
 
-  public boolean isGameOver() {
+
+  public boolean isOver() {
     return state==LOST || state==WON || state==GIVE_UP;
   }
+  public boolean isCardUp(){return state==CARD_UP;}
+  public boolean isCardPlaced(){return state==CARD_PLACED;}
   public boolean isWaiting(){return state==WAITING;}
+  public boolean isWaitingStriked(){return state==WAITING_STRIKED;}
   public boolean isGiveUp(){return state==GIVE_UP;}
   public boolean isWon(){return state==WON;}
   public boolean isLost(){return state==LOST || state==GIVE_UP;}
+  public int getStrikes() {return strikes;}
+
+  private void requireState(int shouldBe) {
+    if (state!=shouldBe)
+      throw new IllegalStateException("State should be:s "+shouldBe+"; is: "+state);
+  }
 
 }
