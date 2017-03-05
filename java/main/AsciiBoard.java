28bc4d0fadeca4f018c564928ad15cfc1918a20e
zaboople <troymotte@yahoo.com>
2017-03-05 13:45:36 -0600

    No logic change, just docs and simplification

--- java/main/AsciiBoard.java
+++ java/main/AsciiBoard.java
@@ -9,57 +9,81 @@ public class AsciiBoard {
     for (int i=0; i<width -1 + (width*3); i++) app.append('_');
     app.append('\n');
     for (int r=0; r<height; r++){
-      for (int innerLine=0; innerLine<3; innerLine++) {
+      for (int innerRow=0; innerRow<3; innerRow++) {
+        // There are three "inner" rows for each row. We render these
+        // one a time, going across all columns before going to the
+        // next "inner row":
         app.append('|');
         for (int c=0; c<width; c++){
           boolean first=board.isStart(r, c);
           boolean last=!first && board.isFinish(r, c);
-          doInnerLine(app, innerLine, board.getCell(r, c), first, last);
+          Cell cell=board.getCell(r, c);
+          doInnerRow(app, innerRow, cell, first, last);
         }
         app.append("\n");
       }
     }
   }
 
-  private static void doInnerLine(
-      Appendable app, int innerLine, Cell cell,
+  /**
+   * This draws one of three lines for a cell: Top, Middle, or Bottom.
+   * It does NOT draw the first character of every line, which is "|".
+   * It DOES draw the last character of every line - also "|".
+   *
+   * The bottom inner row forms the top of the next line, or at the very end,
+   * the bottom of the board.
+   *
+   * The very first line of the board (just a series of _'s) is rendered elsewhere.
+   *
+   * I originally thought it would be a good idea to draw a - left-right across cells
+   * for left-right plays, but that's hard and it doesn't actually look good.
+   */
+  private static void doInnerRow(
+      Appendable app, int innerRow,
+      Cell cell,
       boolean veryFirst, boolean veryLast
     ) throws Exception {
     final char corner=veryFirst || veryLast ?'*' :' ';
     final char bottomCorner=veryFirst || veryLast ?'*' :'_';
-    switch (innerLine) {
+    switch (innerRow) {
       case 0:
         app.append(corner)
           .append(getEdge(cell, Card::hasPathUp, "|", " "))
-          .append(corner);
+          .append(corner)
+          .append("|");
         break;
       case 1:
         app.append(getEdge(cell, Card::hasPathLeft, "-", " "))
           .append(getCenter(cell, veryFirst, veryLast))
-          .append(getEdge(cell, Card::hasPathRight, "-", " "));
+          .append(getEdge(cell, Card::hasPathRight, "-", " "))
+          .append('|');
         break;
       case 2:
         app.append(bottomCorner)
           .append(getEdge(cell, Card::hasPathDown, "|", "_"))
-          .append(bottomCorner);
+          .append(bottomCorner)
+          .append("|");
         break;
     }
-    app.append("|");
   }
 
 
   private static String getEdge(Cell cell, Function<Card, Boolean> checker, String ifSo, String ifNot) {
-    if (cell.isEmpty()) return ifNot;
-    if (!cell.isCard()) return ifNot;
-    if (!checker.apply(cell.getCard())) return ifNot;
-    return ifSo;
+    return checkCard(cell, checker) ?ifSo :ifNot;
   }
+  private static boolean checkCard(Cell cell, Function<Card, Boolean> checker) {
+    return cell!=null &&
+      !cell.isEmpty() &&
+      cell.isCard()   &&
+      checker.apply(cell.getCard());
+  }
+
   private static char getCenter(Cell cell, boolean veryFirst, boolean veryLast) {
     if (veryFirst) return 'S';
     if (veryLast) return 'F';
     if (cell.isEmpty()) return ' ';
     if (cell.isBonus()) return 'B';
     if (cell.isKey()) return 'K';
-    return '*';
+    return '+';
   }
 }
